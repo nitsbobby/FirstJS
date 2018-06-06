cf0371b055e339d76f512709042fee0636ef7d7f
nitsbobby <NITINSHARMA@9.199.34.225>
2018-06-06 16:30:37 +0530

    Main class added.

---
+++ Angular2.0/src/com/angular2/examples/FirstAngular2Class.java
@@ -0,0 +1,9 @@
+package com.angular2.examples;
+
+public class FirstAngular2Class {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello from the Angular 2.0 project");
+	}
+
+}
